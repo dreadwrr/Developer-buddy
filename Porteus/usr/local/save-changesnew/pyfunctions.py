b5b2e7c3eec880e4be233e56daa401103e98b303
Colby Saigeon <csaig929@proton.me>
2025-09-16 05:17:20 -0700

    Add files via upload

--- Porteus/usr/local/save-changesnew/pyfunctions.py
+++ Porteus/usr/local/save-changesnew/pyfunctions.py
@@ -167,7 +167,9 @@ def unescf_py(escaped):
     return s
 
 def parse_line(line):
-    quoted_match = re.search(r'"((?:[^"\\]|\\.)*)"', line)
+    quoted_match = re.search(r'"((?:[^"\\]|\\.)*)"', line)    #     filepath = codecs.decode(raw_filepath.encode(), 'unicode_escape')
+    # except UnicodeDecodeError:
+    #     filepath = raw_filepath
     if not quoted_match:
         return None
     raw_filepath = quoted_match.group(1)
